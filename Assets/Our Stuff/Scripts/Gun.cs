d6513279ad802e11fcb0223a83beed391d62a1d0
Amitvaknin1 <74488893+Amitvaknin1@users.noreply.github.com>
2022-12-11 20:57:38 +0200

    trajetory ++

--- Assets/Our Stuff/Scripts/Gun.cs
+++ Assets/Our Stuff/Scripts/Gun.cs
@@ -36,6 +36,15 @@ public class Gun : MonoBehaviour
     [SerializeField] Transform           cam;
     [Tooltip("Reference to the cinemachine")]
     public CinemachineFreeLook cinemachine;
+    [Header("Projection")]
+    [SerializeField]
+    [Range(10, 100)]
+    private int linepoints = 25;
+    [SerializeField]
+    [Range(0.01f, 0.25f)]
+    private float timeBetweenPoints = 0.1f;
+    [SerializeField] LineRenderer lineRenderer;
+    [SerializeField] LayerMask layer;
 
     [SerializeField] TMP_Text Info;
     //Event
@@ -53,6 +62,7 @@ public class Gun : MonoBehaviour
         _loop += Shoot;
         _loop += Aim;
         _loop += AmmoSwitching;
+        _loop += DrawProjection;
         cinemachine.m_Lens.FieldOfView = NotAimingFOV;
     }
 
@@ -150,6 +160,30 @@ public class Gun : MonoBehaviour
                 }
             }
             CurrentAmmo = AmmoTypes[_currentAmmo];
+        }
+    }
+    void DrawProjection()
+    {
+        lineRenderer.enabled = true;
+        lineRenderer.positionCount = Mathf.CeilToInt(linepoints / timeBetweenPoints) + 1;
+        Vector3 StartPosition = barrel.position;
+        Vector3 StartVelocity = 100 * barrel.forward / 1;
+        int i = 0;
+        lineRenderer.SetPosition(i, StartPosition);
+        for (float time = 0; time < linepoints; time += timeBetweenPoints)
+        {
+            i++;
+            Vector3 point = StartPosition + time * StartVelocity;
+            point.y = StartPosition.y + StartVelocity.y * time + (Physics.gravity.y / 2f * time * time);
+            lineRenderer.SetPosition(i, point);
+            Vector3 lastPostion = lineRenderer.GetPosition(i - 1);
+            if (Physics.Raycast(lastPostion, (point - lastPostion).normalized,
+                out RaycastHit hit, (point - lastPostion).magnitude, layer))
+            {
+                lineRenderer.SetPosition(i, hit.point);
+                lineRenderer.positionCount = i + 1;
+                return;
+            }
         }
     }
 
