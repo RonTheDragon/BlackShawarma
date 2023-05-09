e7381a0c85fe79377a896ba1df802881c7dc916d
RonTheDragon <gweta.ron@gmail.com>
2023-05-09 17:50:41 +0300

    Revert "coffee buff"
    This reverts commit 4dd65a2b1e1fafe64accb0294b129a005837245f.

--- Assets/Our Stuff/Scripts/Shooting/Gun.cs
+++ Assets/Our Stuff/Scripts/Shooting/Gun.cs
@@ -8,7 +8,6 @@ public class Gun : MonoBehaviour
     //Serializefield 
     [Tooltip("shooting cooldown")]
     [SerializeField] float CoolDown = 0.5f;
-    [SerializeField] float CoffeeCD = 0.25f;
 
     [Header("Aiming")]
     [Tooltip("Field of view while aiming")]
@@ -21,8 +20,6 @@ public class Gun : MonoBehaviour
     [ReadOnly][SerializeField] float CurrentFOV;
     [Tooltip("Is the character aiming or not")]
     [ReadOnly][SerializeField] bool  isAiming;
-    [Tooltip("Player Coffee buff")]
-    [SerializeField] public bool _coffee = false;
     [Tooltip("Too Close To Aim At")]
     [SerializeField] private float _tooClose = 3;
     [Tooltip("Too Close To Aim At")]
@@ -127,7 +124,7 @@ public class Gun : MonoBehaviour
 
     void Update()
     {
-        _loop?.Invoke();    
+        _loop?.Invoke();
     }
 
     private void FixedUpdate()
@@ -165,24 +162,9 @@ public class Gun : MonoBehaviour
             }
             else
             {
-                if (CurrentAmmoType.CurrentAmmo > 0 && !_coffee)
+                if (CurrentAmmoType.CurrentAmmo > 0)
                 {
                     _cd = CoolDown;
-
-                    if (!tpm.FreeRoam)
-                    {
-                        GameObject bullet = ObjectPooler.Instance.SpawnFromPool(CurrentAmmoType.AmmoTag, barrel.position, barrel.rotation);
-                        CurrentAmmoType.CurrentAmmo--;
-                        ammoChanged();
-                        float r = isAiming ? _recoil / 3 : _recoil;
-                        tpm.AddLookTorque(Vector2.down, r);
-                        _cis.GenerateImpulse(r);
-                        ShootImpact();
-                    }
-                }
-                if (CurrentAmmoType.CurrentAmmo > 0 && _coffee)
-                {
-                    _cd = CoffeeCD;
 
                     if (!tpm.FreeRoam)
                     {
